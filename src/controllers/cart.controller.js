5ec662e707db5b717c7e634f6ee472626244ba70
JAYESHTAPDIYA12 <jayesh121220@gmail.com>
2025-09-25 18:50:51 +0530

    [jayesh tapdiya] udpated the cart controller

--- src/controllers/cart.controller.js
+++ src/controllers/cart.controller.js
@@ -1,55 +1,66 @@
 import * as cartService from '../services/cart.service';
-import HttpStatus from 'http-status-codes'
+import HttpStatus from 'http-status-codes';
 
-
-
-export const getCart = async (req, res, next) => {
+export const getCart = async (req, res) => {
     try {
         const data = await cartService.getCart(req.body);
-        res.status(HttpStatus.ACCEPTED).json({
-            code: HttpStatus.ACCEPTED,
-            data: data,
-            message: "your cart"
-        })
+        res.status(HttpStatus.OK).json({
+            code: HttpStatus.OK,
+            data,
+            message: "Your cart"
+        });
     } catch (error) {
         res.status(HttpStatus.BAD_REQUEST).json({
             code: HttpStatus.BAD_REQUEST,
-            message: `${error}`
+            message: error.message || "Failed to fetch cart"
         });
     }
-}
+};
 
-export const addBook = async (req, res, next) => {
+export const getBook = async (req, res) => {
     try {
-        const data = await cartService.addBook(req.params, req.body);
-        res.status(HttpStatus.ACCEPTED).json({
-            code: HttpStatus.ACCEPTED,
-            data: data,
-            message: "Book is Added in the cart"
-        })
-
+        const data = await cartService.getBook(req.params._id, req.body);
+        res.status(HttpStatus.OK).json({
+            code: HttpStatus.OK,
+            data,
+            message: "Book in your cart"
+        });
     } catch (error) {
         res.status(HttpStatus.BAD_REQUEST).json({
             code: HttpStatus.BAD_REQUEST,
-            message: `${error}`
+            message: error.message || "Failed to fetch book"
         });
     }
-}
+};
 
-
-export const removeBook = async (req, res, next) => {
+export const addBook = async (req, res) => {
     try {
-        const data = await cartService.removeBook(req.params, req.body);
-        res.status(HttpStatus.ACCEPTED).json({
-            code: HttpStatus.ACCEPTED,
-            data: data,
-            message: "Book is removed in the cart"
-        })
-
+        const data = await cartService.addBook(req.params._id, req.body);
+        res.status(HttpStatus.CREATED).json({
+            code: HttpStatus.CREATED,
+            data,
+            message: "Book is added in the cart"
+        });
     } catch (error) {
         res.status(HttpStatus.BAD_REQUEST).json({
             code: HttpStatus.BAD_REQUEST,
-            message: `${error}`
+            message: error.message || "Failed to add book"
         });
     }
-}
+};
+
+export const removeBook = async (req, res) => {
+    try {
+        const data = await cartService.removeBook(req.params._id, req.body);
+        res.status(HttpStatus.OK).json({
+            code: HttpStatus.OK,
+            data,
+            message: "Book is removed from the cart"
+        });
+    } catch (error) {
+        res.status(HttpStatus.BAD_REQUEST).json({
+            code: HttpStatus.BAD_REQUEST,
+            message: error.message || "Failed to remove book"
+        });
+    }
+};
