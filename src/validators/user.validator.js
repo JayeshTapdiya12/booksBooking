9955a3be872c2bced8a89d349ef609ac6ddd3ccb
JAYESHTAPDIYA12 <jayesh121220@gmail.com>
2025-09-25 18:45:32 +0530

    [jayesh tapdiya]: updated the new userValidator

--- src/validators/user.validator.js
+++ src/validators/user.validator.js
@@ -2,10 +2,10 @@ import Joi from '@hapi/joi';
 
 export const newUserValidator = (req, res, next) => {
   const schema = Joi.object({
-    name: Joi.string().min(3).max(100).required(),
-    lname: Joi.string().min(4).max(100).required(),
+    name: Joi.string().min().required(),
+    lname: Joi.string().min().required(),
     email: Joi.string().email().required(),
-    password: Joi.string().min(8).pattern(new RegExp("^[a-zA-Z0-9@]{3,30}$")).required(),
+    password: Joi.string().min().pattern(new RegExp("^[a-zA-Z0-9@]{3,30}$")).required(),
     mobile: Joi.number().required()
   });
   const { error, value } = schema.validate(req.body);
