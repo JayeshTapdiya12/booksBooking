3570d5e517bbda3489a81de2800b0b3472af272e
JAYESHTAPDIYA12 <jayesh121220@gmail.com>
2025-09-25 18:51:17 +0530

    [jayesh tapdiya] updated the user routing

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,16 +1,16 @@
 import express from 'express';
 import * as userController from '../controllers/user.controller';
 import { newUserValidator } from '../validators/user.validator';
-import { userAuth, roleMiddleware } from '../middlewares/auth.middleware';
+import { userAuth } from '../middlewares/auth.middleware';
 
 const router = express.Router();
 
 
 
 //route to create a new user
-router.post('/sign', newUserValidator, roleMiddleware, userController.sign);
+router.post('/sign', userController.sign);
 
-router.post('/sign/admin', newUserValidator, roleMiddleware, userController.sign);
+// router.post('/sign/admin', newUserValidator, roleMiddleware, userController.sign);
 
 // Route ti get the login by user
 router.post('/login', userController.login)
